bd1133e6e06945b9e4229ed9adcce8cdbcf6fb3f
StoicaLaurentiu <laurentziudmm@gmail.com>
2022-01-21 11:34:49 +0200

    Sub Strings

--- strings.php
+++ strings.php
@@ -13,15 +13,25 @@
     echo "$phrase<br>";
     #strtolower Add Capital Letters to normal
     echo strtolower($phrase);
+    echo "<br>";
     #strtoupper Add Letters to Upper Case
     echo strtoupper($phrase);
+    echo "<br>";
     #String length
     echo strlen($phrase);#Prints 15
+    echo "<br>";
     #This is index []
     echo $phrase[0]; #Prints A
     echo $phrase[1]; #Prints c
     echo "Mike"[1]; #Prints i
+    echo "<br>";
+    echo "<hr>";
+    echo "<br>";
 
+    $phrase = "Hi Academy";
+    echo str_replace("Hi","Panda",$phrase);
+    echo "<br>";
+    echo $phrase;
 
     ?>
 
