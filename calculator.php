5562f745579b127635ab78aba5553442b7b9e4ee
StoicaLaurentiu <laurentziudmm@gmail.com>
2022-01-21 12:41:53 +0200

    Make PHP Calculator

--- calculator.php
+++ calculator.php
@@ -7,11 +7,15 @@
     <body>
 
     <form action="calculator.php" method="get">
+        <input type="number" name="num1">
+        <br>
+        <input type="number" name="num2">
+        <br>
 
         <input type="submit">
     </form>
 
-    <?php  ?>
+    Answer: <?php  echo $_GET["num1"] + $_GET["num2"]?>
 
 
     </body>
