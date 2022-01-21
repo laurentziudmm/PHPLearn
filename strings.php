b5e0bd98b32d8acbe9068ef76f8e3c1b7c3a3a4a
StoicaLaurentiu <laurentziudmm@gmail.com>
2022-01-21 11:27:38 +0200

    Strings

--- strings.php
+++ strings.php
@@ -7,15 +7,20 @@
     <body>
 
     <?php
-    $phrase = " Academy Giraffe";
+    $phrase = "Academy Giraffe";
+    $phrase[0] = "B";
     echo "Fifth year Academy <br>";
-    echo $phrase;
+    echo "$phrase<br>";
     #strtolower Add Capital Letters to normal
     echo strtolower($phrase);
     #strtoupper Add Letters to Upper Case
     echo strtoupper($phrase);
     #String length
-    echo strlen($phrase);
+    echo strlen($phrase);#Prints 15
+    #This is index []
+    echo $phrase[0]; #Prints A
+    echo $phrase[1]; #Prints c
+    echo "Mike"[1]; #Prints i
 
 
     ?>
