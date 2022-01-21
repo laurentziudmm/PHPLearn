62b694b4f4e14d9a7a2050f9a76a7cbc05d605ce
StoicaLaurentiu <laurentziudmm@gmail.com>
2022-01-21 12:25:44 +0200

    User Input

--- userInput.php
+++ userInput.php
@@ -6,11 +6,18 @@
     </head>
     <body>
 
-    <?php
-
-
-    ?>
-
+    <form action="userInput.php" method="get">
+        Name:     <input type="text" name="username"><br>
+        Password: <input type="text" name="password"><br>
+        Email:    <input type="text" name="email"><br>
+        Age:    <input type="number" name="age"><br>
+        <input type="submit">
+    </form>
+    <br>
+    Your name is <?php echo $_GET["username"]?><br>
+    Your password is <?php echo $_GET["password"]?><br>
+    Your email is <?php echo $_GET["email"]?><br>
+    Your age is <?php echo $_GET["age"]?><br>
 
     </body>
 </html>
