b537be6a7bfb7d6f1a74b9bae02b49235f6fde50
StoicaLaurentiu <laurentziudmm@gmail.com>
2022-01-21 12:30:40 +0200

    Make PHP Calculator

---
+++ calculator.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8">
+    <title>   </title>
+    </head>
+    <body>
+
+    <form action="calculator.php" method="get">
+
+        <input type="submit">
+    </form>
+
+    <?php  ?>
+
+
+    </body>
+</html>
