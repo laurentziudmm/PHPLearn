28b5c6662061398d01e2777ab37d1fa4d0f5e4ad
StoicaLaurentiu <laurentziudmm@gmail.com>
2022-01-21 11:52:49 +0200

    Numbers

--- numbers.php
+++ numbers.php
@@ -7,6 +7,19 @@
     <body>
 
     <?php
+    echo 40; #Prints 40
+    echo "<br>";
+    echo 5 + 9; #Prints 14
+    echo "<br>";
+    echo 5.7 * 9; #Prints 51.3
+    echo "<br>";
+    echo 10 % 3; #Prints 1
+    echo "<br>";
+    echo 4 + 5 * 10; #Prints 54
+    echo "<br>";
+    echo (4 + 5) * 10; #Prints 90
+    echo "<br>";
+
 
 
     ?>
