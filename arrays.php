8ca49c4343df9ad85cfd7be102f754b107852014
StoicaLaurentiu <laurentziudmm@gmail.com>
2022-01-26 11:01:33 +0200

    Arrays

--- arrays.php
+++ arrays.php
@@ -5,7 +5,19 @@
     <title>   </title>
     </head>
     <body>
-    <?php   ?>
+    <?php
+        $friends = array("Kevin", "Karen","Oscar", "Laurentiu", "Raluca");
+                            #0      #1      #2          #3          #4
+        echo $friends[0]; #Prints Kevin
+        echo $friends[1]; #Prints Karen
+        $friends[1] = "Katie";
+        echo $friends[1]; #Prints Katie
+        $friends[1] = 400;
+        echo $friends[1]; #Prints 400
+        $friends[5] = "Angela"; #Add Angela to array on position 5
+        echo $friends[5]; #Prints Angela
+        echo count ($friends); # PRints 6 elements on the array
+    ?>
 
     </body>
 </html>
