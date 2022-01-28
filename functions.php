19e80ed2355d0bda91c0b6a1b96929267b293ca0
StoicaLaurentiu <laurentziudmm@gmail.com>
2022-01-28 09:00:18 +0200

    Functions

--- functions.php
+++ functions.php
@@ -8,8 +8,12 @@
 
     <?php
 
+    $colors = array("Red", "Green", "Blue", "Yellow", "Orange");
 
-
+    // Loop through colors array
+    foreach($colors as $value) {
+        echo $value . "<br>";
+    }
     ?>
 
 
